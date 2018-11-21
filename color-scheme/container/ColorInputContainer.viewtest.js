41b340a013bd0a7a1ec038935627849bd4f75f42
jmthompson2015 <softwaredev@jeffreythompson.net>
2018-11-21 10:59:55 -0700

    Moved the code for Color Scheme under color-scheme.

--- color-scheme/container/ColorInputContainer.viewtest.js
+++ color-scheme/container/ColorInputContainer.viewtest.js
@@ -1,8 +1,11 @@
+import ActionCreator from "../state/ActionCreator.js";
+import Color from "../state/Color.js";
 import Reducer from "../state/Reducer.js";
 
 import ColorInputContainer from "./ColorInputContainer.js";
 
 const store = Redux.createStore(Reducer.root);
+store.dispatch(ActionCreator.setColor(Color.RED));
 
 const container = React.createElement(ColorInputContainer);
 const element = React.createElement(ReactRedux.Provider, { store }, container);
--- color-scheme/container/ColorPanelContainer.viewtest.js
+++ color-scheme/container/ColorPanelContainer.viewtest.js
@@ -1,8 +1,11 @@
+import ActionCreator from "../state/ActionCreator.js";
+import Color from "../state/Color.js";
 import Reducer from "../state/Reducer.js";
 
 import ColorPanelContainer from "./ColorPanelContainer.js";
 
 const store = Redux.createStore(Reducer.root);
+store.dispatch(ActionCreator.setColor(Color.RED));
 
 const container = React.createElement(ColorPanelContainer);
 const element = React.createElement(ReactRedux.Provider, { store }, container);
