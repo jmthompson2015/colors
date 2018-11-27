090a67babba3dbacb14d81368c848d7317458da9
jmthompson2015 <softwaredev@jeffreythompson.net>
2018-11-26 17:04:48 -0700

    Changed the color tables to use a flexbox.

--- color-wheel/app/ColorWheel.js
+++ color-wheel/app/ColorWheel.js
@@ -1,14 +1,22 @@
 import Color from "../state/Color.js";
 
 import ColorTable from "../view/ColorTable.js";
+import RU from "../view/ReactUtilities.js";
+
+const element0 = ReactDOMFactories.img({
+  src: "../resource/RBG_color_wheel.png",
+  width: 300,
+  height: 260
+});
 
 const colors1 = [Color.RED, Color.GREEN, Color.BLUE, Color.MAROON, Color.DARK_GREEN, Color.NAVY];
+const title1 = ReactDOMFactories.h2({}, "Primary Colors");
 const element1 = React.createElement(ColorTable, { colors: colors1 });
-ReactDOM.render(element1, document.getElementById("panel1"));
 
 const colors2 = [Color.YELLOW, Color.CYAN, Color.MAGENTA, Color.OLIVE, Color.TEAL, Color.PURPLE];
+const title2 = ReactDOMFactories.h2({}, "Secondary Colors");
+const subtitle2 = ReactDOMFactories.span({}, "Formed by the addition of primary colors.");
 const element2 = React.createElement(ColorTable, { colors: colors2 });
-ReactDOM.render(element2, document.getElementById("panel2"));
 
 const colors3 = [
   Color.ORANGE,
@@ -18,5 +26,19 @@ const colors3 = [
   Color.VIOLET,
   Color.ROSE
 ];
+const title3 = ReactDOMFactories.h2({}, "Tertiary Colors");
+const subtitle3 = ReactDOMFactories.span(
+  {},
+  "Formed by the addition of a primary and a secondary color."
+);
 const element3 = React.createElement(ColorTable, { colors: colors3 });
-ReactDOM.render(element3, document.getElementById("panel3"));
+
+const cells = [
+  RU.createCell(element0, "element0", "pa2"),
+  RU.createCell([title1, element1], "element1", "pa2"),
+  RU.createCell([title2, subtitle2, element2], "element2", "pa2"),
+  RU.createCell([title3, subtitle3, element3], "element3", "pa2")
+];
+
+const element = RU.createFlexboxWrap(cells, "panel", "pa2");
+ReactDOM.render(element, document.getElementById("panel"));
--- color-wheel/state/Color.js
+++ color-wheel/state/Color.js
@@ -14,25 +14,21 @@ Color.GRAY = Color.create({ name: "Gray", s: 0 });
 Color.WHITE = Color.create({ name: "White", s: 0, l: 100 });
 
 Color.RED = Color.create({ name: "Red", h: 0 });
-Color.ORANGE = Color.create({ name: "Orange", h: 30, components: "\u00BD (red + yellow)" });
+Color.ORANGE = Color.create({ name: "Orange", h: 30, components: "red + yellow" });
 Color.YELLOW = Color.create({ name: "Yellow", h: 60, components: "red + green" });
 Color.CHARTREUSE_GREEN = Color.create({
   name: "Chartreuse Green",
   h: 90,
-  components: "\u00BD (yellow + green)"
+  components: "yellow + green"
 });
 Color.GREEN = Color.create({ name: "Green", h: 120 });
-Color.SPRING_GREEN = Color.create({
-  name: "Spring Green",
-  h: 150,
-  components: "\u00BD (green + cyan)"
-});
+Color.SPRING_GREEN = Color.create({ name: "Spring Green", h: 150, components: "green + cyan" });
 Color.CYAN = Color.create({ name: "Cyan", h: 180, components: "green + blue" });
-Color.AZURE = Color.create({ name: "Azure", h: 210, components: "\u00BD (cyan + blue)" });
+Color.AZURE = Color.create({ name: "Azure", h: 210, components: "cyan + blue" });
 Color.BLUE = Color.create({ name: "Blue", h: 240 });
-Color.VIOLET = Color.create({ name: "Violet", h: 270, components: "\u00BD (blue + magenta)" });
+Color.VIOLET = Color.create({ name: "Violet", h: 270, components: "blue + magenta" });
 Color.MAGENTA = Color.create({ name: "Magenta", h: 300, components: "blue + red" });
-Color.ROSE = Color.create({ name: "Rose", h: 330, components: "\u00BD (magenta + red)" });
+Color.ROSE = Color.create({ name: "Rose", h: 330, components: "magenta + red" });
 
 Color.MAROON = Color.create({ name: "Maroon", l: 25, components: "\u00BD red" });
 Color.DARK_GREEN = Color.create({ name: "Dark Green", h: 120, l: 25, components: "\u00BD green" });
